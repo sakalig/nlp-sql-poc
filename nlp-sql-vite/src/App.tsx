b145a2da47a099dc735f12185b3a67d1f5652f45
Gabriel Sakali <gabedevs@gmail.com>
2023-11-10 12:11:35 +0300

    Functional search and filter

--- nlp-sql-vite/src/App.tsx
+++ nlp-sql-vite/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import '@mantine/core/styles.css'
-import {MantineProvider, ColorSchemeScript, Box } from "@mantine/core";
+import {MantineProvider, ColorSchemeScript } from "@mantine/core";
 import 'mantine-datatable/styles.layer.css';
 import './layout.css';
-import {DataTable} from "mantine-datatable";
+import {SearchingAndFilteringExample} from "./SearchingAndFilteringExample.tsx";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,49 +15,7 @@ function App() {
       </head>
       <body>
       <MantineProvider>
-        <DataTable
-            withTableBorder
-            borderRadius="sm"
-            withColumnBorders
-            striped
-            highlightOnHover
-            // provide data
-            records={[
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-              { id: 1, name: 'Joe Biden', bornIn: 1942, party: 'Democratic' },
-            ]}
-            // define columns
-            columns={[
-              {
-                accessor: 'id',
-                // this column has a custom title
-                title: '#',
-                // right-align column
-                textAlign: 'right',
-              },
-              { accessor: 'name' },
-              {
-                accessor: 'party',
-                // this column has custom cell data rendering
-                render: ({ party }) => (
-                    <Box fw={700} c={party === 'Democratic' ? 'blue' : 'red'}>
-                      {party.slice(0, 3).toUpperCase()}
-                    </Box>
-                ),
-              },
-              { accessor: 'bornIn' },
-            ]}
-            // execute this callback when a row is clicked
-
-        />
+        <SearchingAndFilteringExample />
       </MantineProvider>
       </body>
       </html>
---
+++ nlp-sql-vite/src/data.js
@@ -0,0 +1,38 @@
+export const employees = [
+    {
+        id: 1,
+        firstName: 'Jina',
+        lastName: 'Yangu',
+        department: {
+            name: 'Mabadilisho',
+            company: {
+                name: 'Kampuni'
+            },
+        },
+        birthDate: '2004-12-09',
+    },
+    {
+        id: 2,
+        firstName: 'Jina',
+        lastName: 'Yangu',
+        department: {
+            name: 'Mabadilisho',
+            company: {
+                name: 'Kampuni'
+            },
+        },
+        birthDate: '2004-12-09',
+    },
+    {
+        id: 3,
+        firstName: 'Jina',
+        lastName: 'Yangu',
+        department: {
+            name: 'Mabadilisho',
+            company: {
+                name: 'Kampuni'
+            },
+        },
+        birthDate: '2004-12-09',
+    },
+];
